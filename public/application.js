9f890a6b89d164bacc6bd6f46793d3a0fc62e23e
Regis <boudinot.regis@yahoo.com>
2016-04-02 16:22:51 -0600

    can see all relevant data

--- public/application.js
+++ public/application.js
@@ -2,16 +2,29 @@
 const socket = io()
 const universeCall = document.getElementById('universeCall')
 const sendNewUniverse = document.getElementById('sendNewUniverse')
-const atoms = document.getElementById('atoms')
 
 socket.on("sendNewUniverse", function (message) {
   appendUniverseProps(message.universe)
 })
 
-const appendUniverseProps = (message) => {
+const emptyProps = () => {
   $(electrons).empty()
+  $(atoms).empty()
+  $(neutrons).empty()
+  $(protons).empty()
+}
+
+const populateProps = (i, message) => {
+  $(atoms).append(`<h6>${i}</h6>`)
+  $(electrons).append(`<h6>${message[i].atom.electrons}</h6>`)
+  $(neutrons).append(`<h6>${message[i].atom.nucleus.neutrons}</h6>`)
+  $(protons).append(`<h6>${message[i].atom.nucleus.protons}</h6>`)
+}
+
+const appendUniverseProps = (message) => {
+  emptyProps()
   for(let i = 0; i < message.length; i++) {
-    $(electrons).append(`<p>${message[i].atom.electrons}</p>`)
+    populateProps(i, message)
   }
 }
 
